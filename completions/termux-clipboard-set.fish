3fdc98c5dcbf54ed55238819c84f74834f590f21
Yaksh Bariya <yakshbari4@gmail.com>
2021-06-04 15:05:00 +0530

    Add completions for `termux-clipboard-set`

---
+++ completions/termux-clipboard-set.fish
@@ -0,0 +1,7 @@
+# Completions for `termux-clipboard-set`
+# Author: Yaksh Bariya <yakshbari4@gmail.com>
+#
+# Last updated: June 4 2021, 3:05 PM IST
+
+complete -c termux-clipboard-set -f -d "Modify text in device clipboard from terminal"
+complete -c termux-clipboard-set -s h -d "Get help for termux-clipboard-set"
