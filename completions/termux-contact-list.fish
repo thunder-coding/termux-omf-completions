1b4d84c14921ab77c8a2661bf4a1c267f549f8c2
Yaksh Bariya <yakshbari4@gmail.com>
2021-06-04 15:10:00 +0530

    Add completions for `termux-contact-list`

---
+++ completions/termux-contact-list.fish
@@ -0,0 +1,7 @@
+# Completions for `termux-contact-list`
+# Author: Yaksh Bariya <yakshbari4@gmail.com>
+#
+# Last updated: June 4 2021, 3:10 PM IST
+
+complete -c termux-contact-list -f -d "Get device contact in JSON formar"
+complete -c termux-contact-list -s h -d "Get help for termux-contact-list"
