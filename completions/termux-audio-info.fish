d6055f870faa1e57487dbf77cb449ac3c88c9097
Yaksh Bariya <yakshbari4@gmail.com>
2021-06-04 14:30:00 +0530

    Add completions for `termux-audio-info`

---
+++ completions/termux-audio-info.fish
@@ -0,0 +1,7 @@
+# Completions for `termux-audio-info`
+# Author: Yaksh Bariya <yakshbari4@gmail.com>
+#
+# Last updated: June 4 2021, 2:30 PM IST
+
+complete -c termux-audio-info -f -d "Print audio info to the terminal"
+complete -c termux-audio-info -s h -d "Get help for termux-audio-info"
