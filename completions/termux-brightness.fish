144905b58579c9bcfce048c257511880d4c6d9b2
Yaksh Bariya <yakshbari4@gmail.com>
2021-06-04 14:40:00 +0530

    Add completions for `termux-brightness`

---
+++ completions/termux-brightness.fish
@@ -0,0 +1,6 @@
+# Completions for `termux-brightness`
+# Author: Yaksh Bariya <yakshbari4@gmail.com>
+#
+# Last updated: June 4 2021, 2:40 PM IST
+
+complete -c termux-brightness -f -a auto -d "Keep brightness to auto"
