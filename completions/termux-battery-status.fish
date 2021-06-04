f4d25185ba0d3c481a610ceac6267d2e67cd8c40
Yaksh Bariya <yakshbari4@gmail.com>
2021-06-04 14:35:00 +0530

    Add completions for `termux-battery-status`

---
+++ completions/termux-battery-status.fish
@@ -0,0 +1,7 @@
+# Completions for `termux-battery-status`
+# Author: Yaksh Bariya <yakshbari4@gmail.com>
+#
+# Last updated: June 4 2021, 2:35 PM IST
+
+complete -c termux-battery-status -f -d "Get Status for device battery"
+complete -c termux-battery-status -s h -d "Get help for termux-battery-status"
