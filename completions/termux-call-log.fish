7d52b85282f9409b3f241b3404ff6451dae2ffe7
Yaksh Bariya <yakshbari4@gmail.com>
2021-06-04 14:45:00 +0530

    Add completions for `termux-call-log`

---
+++ completions/termux-call-log.fish
@@ -0,0 +1,9 @@
+# Completions for `termux-call-log`
+# Author: Yaksh Bariya <yakshbari4@gmail.com>
+#
+# Last updated: June 4 2021, 2:45 PM IST
+
+complete -c termux-call-log -f -d "Get device call log in JSON format"
+complete -c termux-call-log -s h -d "Get help for termux-call-log"
+complete -c termux-call-log -s l -d "Limit"
+complete -c termux-call-log -s o -d "Offset"
