164c419491fbb68496ac5cd1d0de05fb7a080250
Yaksh Bariya <yakshbari4@gmail.com>
2021-06-05 12:22:49 +0530

    Add completions for `termux-fix-shebang`

---
+++ completions/termux-fix-shebang.fish
@@ -0,0 +1,5 @@
+# Completions for `termux-fix-shebang`
+# Author: Yaksh Bariya <yakshbari4@gmail.com>
+
+complete -c termux-fix-shebang -d "Rewrite shebangs in specified files for running under Termux"
+complete -c termux-fix-shebang -s h -d "Get help for termux-fix-shebang"
