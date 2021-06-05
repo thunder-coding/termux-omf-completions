571088d4a885d13191d3ad82436c4cb942172814
Yaksh Bariya <yakshbari4@gmail.com>
2021-06-05 12:26:05 +0530

    Add completion for `termux-info`

---
+++ completions/termux-info.fish
@@ -0,0 +1,5 @@
+# Completions for `termux-info`
+# Author: Yaksh Bariya <yakshbari4@gmail.com>
+
+complete -c termux-info -f -d "Print device info useful for debugging"
+complete -c termux-audio-info -s h -d "Get help for termux-info"
