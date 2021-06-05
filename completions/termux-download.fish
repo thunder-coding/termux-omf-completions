f7d892af12bc0788d8390adc99b36b4d2e457256
Yaksh Bariya <yakshbari4@gmail.com>
2021-06-05 11:42:15 +0530

    Add completion for `termux-download`

---
+++ completions/termux-download.fish
@@ -0,0 +1,8 @@
+# Completions for `termux-download`
+# Author: Yaksh Bariya <yakshbari4@gmail.com>
+
+complete -c termux-download -f -d "Download a resource using system download manager"
+complete -c termux-download -s h -d "Get help for termux-download"
+complete -c termux-download -s t -d "Title for download request notification"
+complete -c termux-download -s d -d "Description for download request notification"
+complete -c termux-download -s p -d "Full path to which the file should be downloaded"
