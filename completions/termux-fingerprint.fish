c9e8f488827647e82b3820798ebd8e204882c799
Yaksh Bariya <yakshbari4@gmail.com>
2021-06-05 12:10:31 +0530

    Add completions for `termux-fingerprint`

---
+++ completions/termux-fingerprint.fish
@@ -0,0 +1,10 @@
+# Completions for `termux-fingerprint`
+# Author: Yaksh Bariya <yakshbari4@gmail.com>
+
+complete -c termux-fingerprint -f -d "Use fingerprint sensor on device for authentication"
+complete -c termux-fingerprint -s h -d "Get help for termux-fingerprint"
+complete -c termux-fingerprint -r -s t -d "Title"
+complete -c termux-fingerprint -r -s d -d "Description"
+complete -c termux-fingerprint -r -s s -d "Subtitle"
+complete -c termux-fingerprint -r -s c -d "Cancel button name"
+
