b748d1653e0e2e78a1e03f0e6e781431282ea5c0
Yaksh Bariya <yakshbari4@gmail.com>
2021-06-08 08:42:49 +0530

    Add completions for `termux-infrared-transmit`

---
+++ completions/termux-infrared-transmit.fish
@@ -0,0 +1,6 @@
+# Completions for `termux-infrared-transmit`
+# Author: Yaksh Bariya <yakshbari4@gmail.com>
+
+complete -c termux-infrared-transmit -f -d "Transmit an infrared pattern"
+complete -c termux-infrared-transmit -s h -d "Get help for termux-infrared-transmit"
+complete -c termux-infrared-transmit -s f -d "Frequency Pattern"
