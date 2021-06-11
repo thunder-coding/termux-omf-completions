8dc6c690507bca10f107103424d8d5cc69bbd4e9
Yaksh Bariya <yakshbari4@gmail.com>
2021-06-11 22:26:32 +0530

    Add completions for `termux-media-player`

---
+++ completions/termux-media-player.fish
@@ -0,0 +1,9 @@
+# Completions for `termux-media-player`
+# Author: Yaksh Bariya <yakshbari4@gmail.com>
+
+complete -c termux-media-player -f -d "Termux Media Player"
+complete -c termux-media-player -a help -d "Get help for termux-media-player"
+complete -c termux-media-player -a info -d "Display current playback information"
+complete -c termux-media-player -a play -d "Resume/Play specific media file"
+complete -c termux-media-player -a pause -d "Pauses playback"
+complete -c termux-media-player -a stop -d "Quits playback"
