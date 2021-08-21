a368319e02141d2ad044874cf4af6e81a683dd30
Yaksh Bariya <yakshbari4@gmail.com>
2021-08-21 15:22:02 +0530

    Add completions for `termux-media-scan`

---
+++ completions/termux-media-scan.fish
@@ -0,0 +1,4 @@
+complete -c termux-media-scan -d "Scan the specified file(s) and add it to the media content provider"
+complete -f -c termux-media-scan -s h -d "Display help for termux-media-scan"
+complete -f -c termux-media-scan -s r -d "Scan directories recursively"
+complete -f -c termux-media-scan -s v -d "Increase log level"
