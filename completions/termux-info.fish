54f4bb8ae2e90d0d7f4b75576fdf12871dd96e97
Yaksh Bariya <yakshbari4@gmail.com>
2021-08-21 15:10:34 +0530

    Fix foolish typo

--- completions/termux-info.fish
+++ completions/termux-info.fish
@@ -1,2 +1,2 @@
 complete -c termux-info -f -d "Print device info useful for debugging"
-complete -c termux-audio-info -s h -d "Get help for termux-info"
+complete -c termux-info -s h -d "Get help for termux-info"
