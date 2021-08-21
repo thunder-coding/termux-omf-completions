7153e8456b380b99d28f31d21787bdeb3aaa9f5a
Yaksh Bariya <yakshbari4@gmail.com>
2021-08-21 16:10:04 +0530

    Add completions for `termux-nfc`

---
+++ completions/termux-nfc.fish
@@ -0,0 +1,4 @@
+complete -f -c termux-nfc -d "read/write data from/to NDEF tag"
+complete -x -c termux-nfc -s r -a "short full" -d "Read tag"
+complete -x -c termux-nfc -s w -d "Write information on tag"
+complete -x -c termux-nfc -s t -d "Text for tag"
